b0805113e5f9b77f71236482cb8cc9ea6dcab286
Dani9898 <danieleoliva98@gmail.com>
2021-11-15 13:54:51 +0100

    v-for per le chat

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ var app = new Vue(
     {
         el: "#container",
         data: {
+            
+            inChat: 0,
+
             contatti: [
                 {
                     name: "Michele",
