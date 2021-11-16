0c9dd3eebc42a38302c967af3d0d7edf3d01cdfd
Dani9898 <danieleoliva98@gmail.com>
2021-11-16 12:04:20 +0100

    aggiunte date

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var app = new Vue(
                 {
                     name: "Michele",
                     avatar: "img/avatar_1.jpg",
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: "10/01/2020 15:30:55",
@@ -31,7 +31,7 @@ var app = new Vue(
                 {
                     name: "Fabio",
                     avatar: "img/avatar_2.jpg",
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: "20/03/2020 16:30:00",
@@ -53,7 +53,7 @@ var app = new Vue(
                 {
                     name: "Samuele",
                     avatar: "img/avatar_3.jpg",
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: "28/03/2020 10:10:40",
@@ -75,7 +75,7 @@ var app = new Vue(
                 {
                     name: "Luisa",
                     avatar: "img/avatar_4.jpg",
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: "10/01/2020 15:30:55",
@@ -94,7 +94,6 @@ var app = new Vue(
         methods: {
             cambioChat(indice) {
                 this.inChat = indice;
-                console.log(inChat);
             }
         }            
     }      
