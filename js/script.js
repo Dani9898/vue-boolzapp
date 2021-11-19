16f594fe53b74d220fbee1c8e279ec032ed3086b
Dani9898 <danieleoliva98@gmail.com>
2021-11-19 11:23:52 +0100

    milestone 5 fatta

--- js/script.js
+++ js/script.js
@@ -13,83 +13,91 @@ var app = new Vue(
                 {
                     name: "Michele",
                     avatar: "img/avatar_1.jpg",
-                    visible: true,
                     messages: [
                         {
                             date: "10/01/2020 15:30:55",
                             text: "Hai portato a spasso il cane?",
-                            status: "sent"
+                            status: "sent",
+                            visible: false,
                         },
                         {
                             date: "10/01/2020 15:50:00",
                             text: "Ricordati di dargli da mangiare",
-                            status: "sent"
+                            status: "sent",
+                            visible: false,
                         },
                         {
                             date: "10/01/2020 16:15:22",
                             text: "Tutto fatto!",
-                            status: "received"
+                            status: "received",
+                            visible: false,
                         }
                     ]
                 },
                 {
                     name: "Fabio",
                     avatar: "img/avatar_2.jpg",
-                    visible: true,
                     messages: [
                         {
                             date: "20/03/2020 16:30:00",
                             text: "Ciao come stai?",
-                            status: "sent"
+                            status: "sent",
+                            visible: false,
+
                         },
                         {
                             date: "20/03/2020 16:30:55",
                             text: "Bene grazie! Stasera ci vediamo?",
-                            status: "received"
+                            status: "received",
+                            visible: false,
                         },
                         {
                             date: "20/03/2020 16:35:00",
                             text: "Mi piacerebbe ma devo andare a fare la spesa.",
-                            status: "sent"
+                            status: "sent",
+                            visible: false,
                         }
                     ]
                 },
                 {
                     name: "Samuele",
                     avatar: "img/avatar_3.jpg",
-                    visible: true,
                     messages: [
                         {
                             date: "28/03/2020 10:10:40",
                             text: "La Marianna va in campagna",
-                            status: "received"
+                            status: "received",
+                            visible: false,
                         },
                         {
                             date: "28/03/2020 10:20:10",
                             text: "Sicuro di non aver sbagliato chat?",
-                            status: "sent"
+                            status: "sent",
+                            visible: false,
                         },
                         {
                             date: "28/03/2020 16:15:22",
                             text: "Ah scusa!",
-                            status: "received"
+                            status: "received",
+                            visible: false,
                         }
                     ]
                 },
                 {
                     name: "Luisa",
                     avatar: "img/avatar_4.jpg",
-                    visible: true,
                     messages: [
                         {
                             date: "10/01/2020 15:30:55",
                             text: "Lo sai che ha aperto una nuova pizzeria?",
-                            status: "sent"
+                            status: "sent",
+                            visible: false,
                         },
                         {
                             date: "10/01/2020 15:50:00",
                             text: "Si, ma preferirei andare al cinema",
-                            status: "received"
+                            status: "received",
+                            visible: false,
                         }
                     ]
                 }
@@ -115,13 +123,15 @@ var app = new Vue(
                     const newUserMsg = {
                         date: `${actualData} ${actualOra}`,
                         text: this.newMsg,
-                        status: "sent"
+                        status: "sent",
+                        visible: false
                     };
                     
                     const newAnswerMsg = {
                         date: `${actualData} ${actualOra}`,
                         text: "ok",
-                        status: "received"
+                        status: "received",
+                        visible: false
                     };
 
                     inChat2 = this.inChat
@@ -134,6 +144,22 @@ var app = new Vue(
                         this.contatti[inChat2].messages.push(newAnswerMsg)
                     }, 1000);
                 }
+            },
+            showMsgMenu(i) {
+                // if(!this.$refs.msgSettings[i].className.includes("show-settings")) {
+                //     this.$refs.msgSettings[i].classList.add("show-settings")
+                // } else {
+                //     this.$refs.msgSettings[i].classList.remove("show-settings")
+                // }  
+                           
+                if(this.contatti[this.inChat].messages[i].visible == false) {
+                    this.contatti[this.inChat].messages[i].visible = true;
+                } else {
+                    this.contatti[this.inChat].messages[i].visible = false
+                }
+            },
+            deleteMsg(i) {
+                this.contatti[this.inChat].messages.splice(i, 1);
             }
         }             
     }
